7143d8bfbc18abe3331bd02f4eccedfa0a4c08bd
jawadamin99 <info@jawadamin.com>
2026-09-03 04:08:58 +0500

    practice

--- config/db_connector.py
+++ config/db_connector.py
@@ -1,6 +1,5 @@
 import os
 import sqlite3
-from dotenv import load_dotenv
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DB_PATH = os.path.join(BASE_DIR, "db", "jawad_assignments_db.db")
